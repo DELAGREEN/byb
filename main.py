df375ad2aab3f87a90cb10d18a45599588c18a01
delagreen <delagreen@mail.ru>
2024-08-02 19:17:49 +0300

    bug fix, fix len packet > 1500

--- main.py
+++ main.py
@@ -5,29 +5,29 @@ import re
 def modify_packet(packet):
     if packet.haslayer(TCP) and packet.haslayer(Raw):
         payload = packet[Raw].load.decode(errors='ignore')
-        
-        # Фрагментация первого пакета данных на уровне TCP
-        if packet[TCP].seq == 0:
-            fragments = fragment(payload, fragsize=8)
-            for frag in fragments:
-                send(frag)
-            return
-        
+
         # Замена Host заголовка на hoSt
         payload = re.sub(r'Host:', 'hoSt:', payload)
-        
+
         # Удаление пробела между именем заголовка и значением в Host заголовке
         payload = re.sub(r'Host: ', 'Host:', payload)
-        
+
         # Добавление дополнительного пространства между HTTP-методом и URI
         payload = re.sub(r'(GET|POST) /(.*)', r'\1  /\2', payload)
-        
+
         packet[Raw].load = payload.encode()
         del packet[IP].len
         del packet[IP].chksum
         del packet[TCP].chksum
-        send(packet)
-        
+
+        # Проверка и фрагментация пакета, если его размер превышает MTU
+        if len(packet) > 1500:  # Предполагается, что 1500 - это размер MTU
+            fragments = fragment(packet, fragsize=1440)  # 1440 для учёта заголовков IP и TCP
+            for frag in fragments:
+                send(frag)
+        else:
+            send(packet)
+
 # Захват и модификация пакетов
 def packet_callback(packet):
     if packet.haslayer(TCP):
@@ -35,7 +35,7 @@ def packet_callback(packet):
 
 # Установить правила iptables для перенаправления трафика на локальный порт 9999
 import os
-os.system('sudo iptables -t nat -A OUTPUT -p tcp --dport 80 -j REDIRECT --to-port 9999')
+os.system('iptables -t nat -A OUTPUT -p tcp --dport 80 -j REDIRECT --to-port 9999')
 
 # Запуск захвата пакетов
 sniff(prn=packet_callback, filter='tcp', store=0)
